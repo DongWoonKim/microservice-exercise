5d99c357933c4c4b0127cd67e572ebd3bb1de916
DongwoonKim <01030985843z@naver.com>
2021-12-02 15:15:55 +0900

    microservice building apigateway

---
+++ accountmanagement-microservice-app/src/test/java/com/example/accountmanagementmicroserviceapp/AccountmanagementMicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountmanagementmicroserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagementMicroserviceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ users-microservice-app/src/test/java/com/example/usersmicroserviceapp/UsersMicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersmicroserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMicroserviceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
